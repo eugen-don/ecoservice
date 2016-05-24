20d518ee801dee6949cc2d9a11b096a1ffbabb7e
Christian Schöttke <c.schoettke@ecoservice.de>
2016-05-24 16:57:14 +0200

    [REF] ecoservice_financeinteface, ecoservice_financeinteface_datev, ecoservice_financeinteface_datev_export: Meta refactoring
    Over the course of the general reworking of the finance interface there are some meta updates concerning the general folder and configuration structure of the modules to be done.
    This commit concerns the configuration structure.

--- ecoservice_financeinterface_datev/ecofi.py
+++ ecoservice_financeinterface_datev/ecofi.py
@@ -1,27 +1,8 @@
-# -*- encoding: utf-8 -*-
-""" The ecofi module extends the original OpenERP ecofi objects with different attributes and methods
+# -*- coding: utf-8 -*-
 """
-##############################################################################
-#    ecoservice_financeinterface_datev
-#    Copyright (c) 2013 ecoservice GbR (<http://www.ecoservice.de>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#    This program based on OpenERP.
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-##############################################################################
+The ecofi module extends the original OpenERP ecofi objects with different attributes and methods
+"""
+
 from openerp.osv import orm
 from decimal import Decimal
 from openerp.tools.translate import _
